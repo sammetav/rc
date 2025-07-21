7926bd1b8588cbdfe2fcd29593ce0e6f46bd3ff5
sammetav <vishnu.sammeta@tset.com>
2025-07-21 23:38:25 +0200

    bump

---
+++ src/main/java/streams/rc/RcApplication.java
@@ -0,0 +1,13 @@
+package streams.rc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RcApplication.class, args);
+    }
+
+}
