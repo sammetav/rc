ce94898349a60faaa6c5e81a48752da99089034b
sammetav <vishnu.sammeta@tset.com>
2025-07-22 01:53:37 +0200

    readme

--- src/main/java/streams/rc/repository/PetRepository.java
+++ src/main/java/streams/rc/repository/PetRepository.java
@@ -2,7 +2,7 @@ package streams.rc.repository;
 
 
 import org.springframework.data.repository.reactive.*;
-import reactor.core.publisher.*;
+import reactor.core.publisher.Flux;
 import streams.rc.entity.*;
 
 public interface PetRepository extends ReactiveCrudRepository<Pet, Long> {
